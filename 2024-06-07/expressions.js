568224fac9487eaa6319074a4391051293a82e8e
Peterson V <petersonvgama@gmail.com>
2024-06-07 22:25:18 -0300

    Adicionando material do segundo dia

---
+++ 2024-06-07/expressions.js
@@ -0,0 +1,29 @@
+let a = 50
+console.log(a)
+
+// console.log(let b = 10)
+
+let b = a
+console.log(b)
+
+// Statements com valores são expressões
+// Statements sem valores não são expressões
+
+// let a1 = 1
+let a2 = 1 //+ 2
+let a3 = 1
+
+/*
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+console.log('meu exemplo de trecho grnade')
+*/
+
+console.log('eu serei executado')
---
+++ 2024-06-07/identifiers.js
@@ -0,0 +1,26 @@
+let minhaVariavel
+let minhaVariavel2
+// let 2minhaVariavel
+// let %minhaVariavel
+let minha23132131____$$$dadadad = 5
+
+let pacocaLegal = 10
+console.log(pacocaLegal)
+
+// Snake case
+const snake_case = 10
+const meu_valor_de_snake_case = 10
+
+// Notação hungariana
+const NRminhaVariavel = 10
+const STRminhaOutraVariavel = 'minha outra variavel'
+
+// Camel case
+const minhaVariavel4 = 10
+const minhaOutraVariavel = 'minha outra variavel'
+const minhaVariavelComNomeBemGrandeWtf = 10
+
+// Pascal case
+const MinhaVariavel4 = 10
+const MinhaOutraVariavel = 'minha outra variavel'
+const MinhaVariavelComNomeBemGrandeWtf = 10
