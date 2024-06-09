8074903d995230345a3c714fa1833b053d1dbad8
Peterson V <petersonvgama@gmail.com>
2024-06-09 18:31:31 -0300

    Adicionando exercicios

---
+++ 2024-06-09/01-operadores-simples.js
@@ -0,0 +1,37 @@
+// Declação de variaveis
+let x
+
+// Operação de atribuição
+x = 10
+
+// Declação de variáveis e atribuição na mesma linha
+// (Forma curta)
+let y = 10
+
+let valoreFeitoPorOutrasVariaveis = x * y
+
+// Operador de exponenciação
+
+let resultado = 2 ** 3 // Resultará em 8, que é 2 elevado a 3
+
+let resultado2 = 10 % 2 // resultado 0, pois não sobra nada
+let resultado3 = 11 % 2 // resultado 1, pois sobra 1
+let resultado4 = 12 % 2 // resultado 0, pois não sobra nada
+// Devido a essa caracteristica, é comum usar % para
+// determinar se um número é par ou ímpar (no caso, o 10, 11 ou 12)
+
+let aIncrementar = 10
+console.log(aIncrementar)
+aIncrementar++
+console.log(aIncrementar)
+
+let aDecrementar = 1000
+console.log(aDecrementar)
+aDecrementar--
+console.log(aDecrementar)
+
+let numero = 10
+let outroNumero = 20
+console.log(numero)
+numero += outroNumero // Igual a numero = numero + outroNumero
+console.log(numero)
---
+++ 2024-06-09/bruno.js
@@ -0,0 +1,42 @@
+console.log('Exercicio 1')
+var ex1 = 5 <= 8 //Wynik przykładu 1
+var ex2 = 12 >= -1 //Wynik przykładu 2
+var ex3 = 2 == 2 //Wynik przykładu 3
+var ex4 = 999 != 0.234
+
+console.log(ex1)
+console.log(ex2)
+console.log(ex3)
+console.log(ex4)
+
+console.log('Exercicio 2')
+
+var ex5 = 5 == 8
+var ex6 = 12 == -1
+var ex7 = 2 > 2.01
+var ex8 = 999 < 0.234
+
+console.log(ex5)
+console.log(ex6)
+console.log(ex7)
+console.log(ex8)
+
+console.log('Exercicio 3')
+
+var ex9 = 3 < 4 && 1 < 7 //Result of example 1
+var ex10 = 12 > 5 || 6 > -4 //Result of example 2
+var ex11 = !(12 == 5)
+
+console.log(ex9)
+console.log(ex10)
+console.log(ex11)
+
+console.log('Exercicio 4')
+
+var ex12 = 2 >= 8 && 1 == 1 //Result of example 1
+var ex13 = 0 == 3 || !(13 > -2) //Result of example 2
+var ex14 = !!(12 < 5) //Result of example 3
+
+console.log(ex12)
+console.log(ex13)
+console.log(ex14)
