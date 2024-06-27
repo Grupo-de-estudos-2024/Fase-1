0a3c9f2cb1cf142d67d97ff6cb4ee936385b19bb
Peterson V <petersonvgama@gmail.com>
2024-06-26 21:32:14 -0300

    Adicionando material de while

---
+++ 2024-06-26/00-introducao.js
@@ -0,0 +1,15 @@
+// Fluxo de controle
+// Loops
+// while
+
+// while (true) {
+//   console.log('oi, eu sou o loop infinito')
+// }
+
+let i = 15
+
+while (i < 10) {
+  console.log(i)
+  i++
+}
+console.log('fim do programa')
---
+++ 2024-06-26/01-exercicios.js
@@ -0,0 +1,31 @@
+// 01- Escreva um programa que imprima de 0 até 100
+// para cada número imprima se o número é par ou ímpar
+
+// 02- Escreva um programa que imprima de 0 até 100
+// para cada número, imprima 'buzz' se o número for múltiplo de 3
+// 'fizz' se o número for múltiplo de 5
+// 'fizzbuzz' se o número for múltiplo de 3 e 5
+
+// 03- Escreva um programa que use loops para imprimir o seguinte:
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// 04- Escreva um programa que liste todos os números
+// de 0 até 1000 que sejam múltiplos de 3 e 5
+// Ao final do programa, imprima a soma de todos esses números
+
+// 05- Escreva um programa que use loops para imprimir o seguinte padrão:
+/* 1 
+   1 2 
+   1 2 3 
+   1 2 3 4 
+   1 2 3 4 5 
+   1 2 3 4 5 6 
+   1 2 3 4 5 6 7 
+   1 2 3 4 5 6 7 8 */
+
+// 06- Escreva um programa que receba um número inteiro de parâmetro
+// e use loops para imprimir quantos dígitos um número tem.
