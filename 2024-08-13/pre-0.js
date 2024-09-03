8c1b9b4bfc4eb2a2f775fc056a6803c35c0a3f34
Peterson V <petersonvgama@gmail.com>
2024-09-02 21:57:06 -0300

    Adicionando exercícios de hoje

---
+++ 2024-08-13/pre-0.js
@@ -0,0 +1,16 @@
+// Dado uma versão semântica (v13.3.5)
+// major.minor.patch
+// e o nome de uma atualização (major.minor.patch)
+// gere o próximo número de versão
+
+// v13.3.5
+// patch
+// v13.3.6
+
+// v13.3.5
+// minor
+// v13.4.0
+
+// v13.3.5
+// major
+// v14.0.0
