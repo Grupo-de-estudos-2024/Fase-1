e7784be0aef543efacc47da2ad914ecefbfbe21f
Peterson V <petersonvgama@gmail.com>
2024-09-05 21:33:32 -0300

    Adicionando exercícios de hoje

---
+++ 2024-08-13/33.js
@@ -0,0 +1,38 @@
+// Write a JavaScript program to check whether two
+// numbers are in the range 40..60 or 70..100 inclusive
+
+let numero1 = parseFloat(process.argv[2])
+let numero2 = parseFloat(process.argv[3])
+
+let estaNoIntervalo = false
+// if(numero1 <= 100 && numero2 <= 100){
+//     if(numero1 >= 70 && numero2 >= 70){
+//         console.log("Os numeros estão no intervalo")
+//     } else if((numero1 >= 40 && numero1 <= 60) && (numero1 >= 40 && numero1 <= 60)){
+//         console.log("Os numeros estão no intervalo")
+//     }
+//     console.log("Os numeros estão fora do intervalo")
+// } else{
+//     console.log("Os numeros estao fora do intervalo")
+// }
+
+// 45 60
+// 75 77
+// 50 80
+// 10 110
+// 45 110
+// 20 75
+
+if (numero1 <= 100 && numero2 <= 100) {
+  if (numero1 >= 70 && numero2 >= 70) {
+    estaNoIntervalo = true
+  } else if (numero1 >= 40 && numero1 <= 60 && numero1 >= 40 && numero1 <= 60) {
+    estaNoIntervalo = true
+  }
+}
+
+if (!estaNoIntervalo) {
+  console.log('Os numeros estão no intervalo')
+} else {
+  console.log('Os numeros estao fora do intervalo')
+}
